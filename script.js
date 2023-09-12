ef9018d5c5b394f3f6c461aac3825069f78a6c25
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-12 16:00:13 +0100

    sounds working for each icon

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const drumKitPartsList = document.querySelector("ul");
+const drumKitPartsList = document.querySelector("figure");
 // console.log(drumKitPartsList);
 
 drumKitPartsList.addEventListener("click", (event) => {
@@ -6,15 +6,48 @@ drumKitPartsList.addEventListener("click", (event) => {
   let sound = "";
   switch (event.target.textContent) {
     case "High Tom":
-      sound = new Audio("./sounds/tom.wav");
+      sound = new Audio("./sounds/high_tom.wav");
       sound.play();
       break;
     case "Medium Tom":
-      sound = new Audio("./sounds/tom.wav");
+      sound = new Audio("./sounds/medium_tom.wav");
+      sound.play();
+      break;
+    case "Ride Cymbal":
+      sound = new Audio("./sounds/ride_cymbal.wav");
+      sound.play();
+      break;
+    case "Bass Drum":
+      sound = new Audio("./sounds/bass_drum.wav");
+      sound.play();
+      break;
+    case "Floor Tom":
+      sound = new Audio("./sounds/floor_tom.wav");
+      sound.play();
+      break;
+    case "Stool":
+      sound = new Audio("./sounds/stool.wav");
+      sound.play();
+      break;
+    case "Bass Pedal":
+
+
+
+    break;
+    case "Snare Drum":
+      sound = new Audio("./sounds/snare_drum.wav");
+      sound.play();
+      break;
+    case "Hi-Hats":
+      sound = new Audio("./sounds/hi_hats.wav");
+      sound.play();
+      break;
+    case "Crash Cymbal":
+      sound = new Audio("./sounds/crash_cymbal.wav");
       sound.play();
       break;
     case "Snare Drum":
-      sound = new Audio("./sounds/snare.wav");
+      sound = new Audio("./sounds/snare_drum.wav");
       sound.play();
       break;
     default:
